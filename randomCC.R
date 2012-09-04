ca753124780d3ad0ff2bb90a0c85ca3a7b3d27a9
seventm <konraddebski@gmail.com>
2012-09-05 00:13:22 +0200

    randomCC.R

--- randomCC.R
+++ randomCC.R
@@ -1,11 +1,9 @@
-random_cc<-function(data, size=0.1){
-cc<-NULL
-n<-round(nrow(data)*size)
-a<- sample(1:nrow(data), n)
-L<-c(1:nrow(data))
-a2<-data.frame(L,dane)
-cc<-data.frame(a2[L %in% a,  ])
-cc<-subset(cc,select=-L)
-return(cc)
+random_cc<-function(x, fraction=0.1)
+{
+	size <- nrow(x)
+	newSize <- round(size * fraction)
+	randomRows <- sample(size, newSize)
+	randomObjects <- x[randomRows,]
+	return(randomObjects)
 }
   
