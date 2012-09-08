c262a026923772d9b042725b87df487342cd5666
seventm <konraddebski@gmail.com>
2012-09-08 10:29:56 +0200

    MD.R

--- MD.R
+++ MD.R
@@ -1,7 +1,9 @@
 MD<-function(CC,EC){
-	tmp <- unlist(lapply(1:ncol(CC), function(i, CC, EC){
-		return(colMeans(CC[i]) - colMeans(EC[i])) / colMeans(EC[i])
-	}, CC, EC))
+	colMeans_CC <- colMeans(CC)
+	colMeans_EC <- colMeans(EC)
+	tmp <- unlist( lapply( 1:ncol(CC), function(i, colMeans_CC, colMeans_EC ){
+		return( ( colMeans_CC[i] - colMeans_EC[i] ) / colMeans_EC[i] )
+	}, colMeans_CC, colMeans_EC ) )
 	MD_index <- (sum(tmp) / ncol(CC)) * 100
 	return(MD_index)
 }
