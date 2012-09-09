f32981e454678c3ad9041e3c69a42aacbd3e4606
seventm <konraddebski@gmail.com>
2012-09-10 00:18:55 +0200

    PRO.R

--- PRO.R
+++ PRO.R
@@ -1,5 +1,4 @@
-PRO <- function(groups, size, newSize){
-	grpIDs <- unique(groups)
+PRO <- function(groups, size, newSize, grpIDs){
 	newGrpSizes<-unlist(lapply(1:length(grpIDs), function(i, groups){
 		newSize <- round(newSize*(sum(groups==i)/size))
 		if(newSize == 0)
