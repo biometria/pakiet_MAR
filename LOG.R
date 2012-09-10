5ddfee4537e8b9837f361de8a30e463ed36c78e6
seventm <konraddebski@gmail.com>
2012-09-10 11:06:45 +0200

    LOG.R

--- LOG.R
+++ LOG.R
@@ -1,14 +1,14 @@
-LOG <- function(data, groups, size){
+LOG <- function(groups, size, newSize, grpIDs, grpSize){
 	sumGrpLog<-unlist( lapply( 1:length(grpIDs), function(i, grpSize){
 		return(log(grpSize[i]) * grpSize[i])
 	}, grpSize) )
 	sumLog <- sum(sumGrpLog)
 	
-	newGrpSizes<-lapply( 1: length(grpIDs), function(i, newSize, sumGrpLog, sumLog){
+	newGrpSizes<-unlist( lapply( 1: length(grpIDs), function(i, newSize, sumGrpLog, sumLog){
 		newGrpSize <- round(newSize * ( sumGrpLog[i] / sumLog ))
 		if(newGrpSize == 0)
 			newGrpSize <- 1
 		return(newGrpSize)
-	}, newSize, sumGrpLog, sumLog)
+	}, newSize, sumGrpLog, sumLog) )
 	retutn(newGrpSizes)
 }
